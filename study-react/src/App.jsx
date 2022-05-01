0bff5ec980df7968785f31267740fe89825d48bc
monomonosu <pojinyan@yahoo.co.jp>
2022-05-01 21:21:55 +0900

    React独自のイベント実行について

--- study-react/src/App.jsx
+++ study-react/src/App.jsx
@@ -1,8 +1,13 @@
 export const App = () => {
+    const onClickButton = () => {
+        alert();
+    };
+
     return (
         <div>
             <h1>こんにちは世界！</h1>
             <p>お元気ですか</p>
+            <button onClick={onClickButton}>ボタン</button>
         </div>
     );
 };
