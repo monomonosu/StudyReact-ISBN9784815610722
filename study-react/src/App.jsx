7d555ad59d3c4e476b66f7182c6fb90a94408eed
monomonosu <pojinyan@yahoo.co.jp>
2022-05-01 21:50:03 +0900

    props.childrenについて学ぶ

--- study-react/src/App.jsx
+++ study-react/src/App.jsx
@@ -8,8 +8,8 @@ export const App = () => {
     return (
         <div>
             <h1 style={{ color: "red" }}>こんにちは世界！</h1>
-            <ColoredMessage color="blue" message="お元気ですか"></ColoredMessage>
-            <ColoredMessage color="pink" message="元気です"></ColoredMessage>
+            <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
+            <ColoredMessage color="pink">元気です</ColoredMessage>
             <button onClick={onClickButton}>ボタン</button>
         </div>
     );
