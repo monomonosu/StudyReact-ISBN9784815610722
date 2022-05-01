1ef7596d4269fcaace068a25480d0223cc8055a4
monomonosu <pojinyan@yahoo.co.jp>
2022-05-01 21:15:57 +0900

    App.jsx作成。コンポーネント化。

---
+++ study-react/src/App.jsx
@@ -0,0 +1,8 @@
+export const App = () => {
+    return (
+        <div>
+            <h1>こんにちは世界！</h1>
+            <p>お元気ですか</p>
+        </div>
+    );
+};
--- study-react/src/index.js
+++ study-react/src/index.js
@@ -1,12 +1,4 @@
 import ReactDOM from "react-dom";
-
-const App = () => {
-    return (
-        <div>
-            <h1>こんにちは世界！</h1>
-            <p>お元気ですか</p>
-        </div>
-    );
-};
+import { App } from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"));
