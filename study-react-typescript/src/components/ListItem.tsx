848430c6745305f9ee053fa3871774e9bdf2bf6a
monomonosu <pojinyan@yahoo.co.jp>
2022-05-04 19:43:47 +0900

    FCで型定義されたコンポーネントのprops省略

--- study-react-typescript/src/components/ListItem.tsx
+++ study-react-typescript/src/components/ListItem.tsx
@@ -2,10 +2,14 @@ import type { FC } from "react";
 import type { Music } from "../types/music";
 
 export const ListItem: FC<Music> = props => {
-    const { id, musicName, fileSize = "notData" } = props;
+    const { id, musicName, fileSize } = props;
     return (
         <p>
             {id}:{musicName}({fileSize})
         </p>
     );
+};
+
+ListItem.defaultProps = {
+    fileSize: "notData"
 };
